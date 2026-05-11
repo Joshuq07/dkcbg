4f4c15cf692914d4c84d55d37576a47ae9986b0c
Joshua Patrick <patri198@msu.edu>
2026-05-10 20:37:24 -0400

    lskdflksd

--- digital/src/app/session/[id]/songs/page.tsx
+++ digital/src/app/session/[id]/songs/page.tsx
@@ -2,6 +2,7 @@
 
 //@ts-ignore
 import './dkcbg_music.css';
+import Image from 'next/image'
 
 export default function Music() {
 
@@ -17,29 +18,47 @@ export default function Music() {
           <div className="music-selectg">
             <div className="music-selectg-row">
               <div className="music-selectg-button" data-color="green" data-i="1">
-                <img className="music-selectg-img" src="games/dkc_snes.png" />
+                <Image
+                  src="/games/dkc_snes.png"
+                  alt="DKC SNES"
+                />
                 <div className="music-selectg-game">DKC SNES</div>
               </div>
               <div className="music-selectg-button" data-color="red" data-i="2">
-                <img className="music-selectg-img" src="games/dkc2_snes.png" />
+                <Image
+                  src="/games/dkc2_snes.png"
+                  alt="DKC2 SNES"
+                />
                 <div className="music-selectg-game">DKC2 SNES</div>
               </div>
               <div className="music-selectg-button" data-color="blue" data-i="3">
-                <img className="music-selectg-img" src="games/dkc3_snes.png" />
+                <Image
+                  src="/games/dkc3_snes.png"
+                  alt="DKC2 SNES"
+                />
                 <div className="music-selectg-game">DKC3 SNES</div>
               </div>
             </div>
             <div className="music-selectg-row">
               <div className="music-selectg-button" data-color="green" data-i="4">
-                <img className="music-selectg-img" src="games/dkc_gba.png" />
+                <Image
+                  src="/games/dkc_gba.png"
+                  alt="DKC2 GBA"
+                />
                 <div className="music-selectg-game">DKC GBA</div>
               </div>
               <div className="music-selectg-button" data-color="red" data-i="5">
-                <img className="music-selectg-img" src="games/dkc2_gba.png" />
+                <Image
+                  src="/games/dkc2_gba.png"
+                  alt="DKC2 GBA"
+                />
                 <div className="music-selectg-game">DKC2 GBA</div>
               </div>
               <div className="music-selectg-button" data-color="blue" data-i="6">
-                <img className="music-selectg-img" src="games/dkc3_gba.png" />
+                <Image
+                  src="/games/dkc3_gba.png"
+                  alt="DKC2 GBA"
+                />
                 <div className="music-selectg-game">DKC3 GBA</div>
               </div>
             </div>
@@ -54,7 +73,10 @@ export default function Music() {
             <div className="music-play-back">&lt; Back to Songs</div>
             <div className="music-play-game">DKC SNES</div>
             <div className="music-play-name">Opening Fanfare</div>
-            <img className="music-play-img" src="large/dkc_snes_island.png" />
+            <Image
+              src="/large/dkc_snes_island.png"
+              alt="DKC SNES Island"
+            />
             <video className="music-play-video" width="1200" controls><source src="videos/dkc_snes_theme.mp4" type="video/mp4"></source></video>
           </div>
         </body>
